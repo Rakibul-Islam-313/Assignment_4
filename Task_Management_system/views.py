8cee4cd2f97839ccb0d515a7554d3f5b01f07363
Rakibul Islam <ri3113600@gmail.com>
2024-12-29 09:55:04 +0600

    update

--- Task_Management_system/views.py
+++ Task_Management_system/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from tasks . models import MyTask
 def add_show(request):
     data = MyTask.objects.all()
-    return render(request,'show.html', {'data': data})
+    return render(request,'index.html', {'data': data})
